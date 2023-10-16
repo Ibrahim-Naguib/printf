7fa17fe5304698f7bcf939cef673f17a7cabb680
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-16 18:11:46 +0000

    new commit

--- print_address.c
+++ print_address.c
@@ -1,36 +1,46 @@
 #include "main.h"
 
-/**
- * print_address - Prints a memory address in hexadecimal format.
- *
- * @args: va_list argument containing the pointer to be printed.
- *
- * Return: The number of characters printed.
- */
+static void reverse_print(char *str, int length) {
+    int start = 0;
+    int end = length - 1;
+	char temp;
 
-int print_address(va_list args)
-{
-	void *ptr = va_arg(args, void*);
-	int chars_printed = 0, temp_index = 0, digit, i;
-	char hex_digits[16] = "0123456789abcdef";
-	char temp_buffer[32];
+    while (start < end) {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int print_address(va_list args) {
+    void* ptr = va_arg(args, void*);
+    int chars_printed = 0;
+    char hex_buffer[32];
+    int temp_index = 0;
+	int digit, i;
 	unsigned long int value;
 
-	if (ptr == NULL)
-		return (_printf("(null)"));
-
-	chars_printed += _putchar('0');
-	chars_printed += _putchar('x');
+    if (ptr == NULL) {
+        return _printf("(null)");
+    }
 
 
-	for (value = (unsigned long int)ptr; value > 0; value /= 16)
-	{
-		digit = value % 16;
-		temp_buffer[temp_index++] = hex_digits[digit];
-	}
+    chars_printed += _putchar('0');
+    chars_printed += _putchar('x');
 
-	for (i = temp_index - 1; i >= 0; i--)
-		chars_printed += _putchar(temp_buffer[i]);
-	return (chars_printed);
+    for (value = (unsigned long int)ptr; value > 0; value /= 16) {
+        digit = value % 16;
+        hex_buffer[temp_index++] = (digit < 10) ? ('0' + digit) : ('A' + digit - 10);
+    }
+
+    reverse_print(hex_buffer, temp_index);
+
+    for (i = 0; i < temp_index; i++) {
+        chars_printed += _putchar(hex_buffer[i]);
+    }
+
+    return chars_printed;
 }
 
