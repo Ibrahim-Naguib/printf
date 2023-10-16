be51f4526f71688c55d28282ee2f97191e5f5c53
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-16 17:42:52 +0000

    new commit

--- print_address.c
+++ print_address.c
@@ -10,27 +10,27 @@
 
 int print_address(va_list args)
 {
-    void* ptr = va_arg(args, void*);
-    int chars_printed = 0, temp_index = 0, digit, i;
-    char hex_digits[16] = "0123456789abcdef";
-    char temp_buffer[32];
+	void *ptr = va_arg(args, void*);
+	int chars_printed = 0, temp_index = 0, digit, i;
+	char hex_digits[16] = "0123456789abcdef";
+	char temp_buffer[32];
 	unsigned long int value;
 
-    if (ptr == NULL)
-        return (_printf("(null)"));
+	if (ptr == NULL)
+		return (_printf("(null)"));
 
-    chars_printed += _putchar('0');
-    chars_printed += _putchar('x');
+	chars_printed += _putchar('0');
+	chars_printed += _putchar('x');
 
 
-    for (value = (unsigned long int)ptr; value > 0; value /= 16) 
+	for (value = (unsigned long int)ptr; value > 0; value /= 16)
 	{
-        digit = value % 16;
-        temp_buffer[temp_index++] = hex_digits[digit];
-    }
+		digit = value % 16;
+		temp_buffer[temp_index++] = hex_digits[digit];
+	}
 
-    for (i = temp_index - 1; i >= 0; i--)
-        chars_printed += _putchar(temp_buffer[i]);
-    return (chars_printed);
+	for (i = temp_index - 1; i >= 0; i--)
+		chars_printed += _putchar(temp_buffer[i]);
+	return (chars_printed);
 }
 
